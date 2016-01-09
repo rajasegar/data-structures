9964b96e4dd91a69336f9ae2771447fd526b1b00
rajasegar-c <rajasegarc@freshdesk.com>
2016-01-09 20:49:48 +0530

    exporting Queue

--- src/Queue.js
+++ src/Queue.js
@@ -1,34 +1,32 @@
-(function() {
-    Queue = function() {
-        this.dataStore = [];
-        console.log("Queue created");
-    };
+var Queue = function() {
+    this.dataStore = [];
+    console.log("Queue created");
+};
 
-    Queue.prototype = {
-        enqueue: function(element) {
-            this.dataStore.push(element);
-        },
-        dequeue: function(){
-            return this.dataStore.shift();
-
-        },
-        front: function(){
-            return this.dataStore[0];
-        },
-        back: function(){
-            return this.dataStore[this.dataStore.length - 1];
-        },
-        toString: function(){
-            var retStr = "";
-            var ds_length = this.dataStore.length;
-            for(var i=0; i < ds_length; ++i) {
-                retStr += this.dataStore[i] + "\n";
-            }
-            return retStr;
-        },
-        empty: function(){
-            return (this.dataStore.length === 0) ? true : false;
+Queue.prototype = {
+    enqueue: function(element) {
+        this.dataStore.push(element);
+    },
+    dequeue: function(){
+        return this.dataStore.shift();
+    },
+    front: function(){
+        return this.dataStore[0];
+    },
+    back: function(){
+        return this.dataStore[this.dataStore.length - 1];
+    },
+    toString: function(){
+        var retStr = "";
+        var ds_length = this.dataStore.length;
+        for(var i=0; i < ds_length; ++i) {
+            retStr += this.dataStore[i] + "\n";
         }
+        return retStr;
+    },
+    empty: function(){
+        return (this.dataStore.length === 0) ? true : false;
+    }
+};
 
-    };
-}());
+module.exports = Queue;
