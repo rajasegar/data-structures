64fff639bed4b4641ca2cf5078650df9bec69a29
rajasegar-c <rajasegarc@freshdesk.com>
2016-01-09 17:38:42 +0530

    publishing as a npm package

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
  * @copyright Rajasegar Chandran, 2016.
  */
 
-var List = require('./dist/List.js');
-var Stack = require('./dist/Stack.js');
-var Queue = require('./dist/Queue.js');
+var List = require('./dist/List.min.js');
+var Stack = require('./dist/Stack.min.js');
+var Queue = require('./dist/Queue.min.js');
 
 module.exports = {
     List:List,
